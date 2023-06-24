42d89781d5cd86aa7c1f0d0e8fecbf553b877446
Sufian Idris <sufian.idris@gmail.com>
2023-06-24 13:52:12 +0400

    Initial Commit

---
+++ Counter App/Counter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_AppApp.swift
+//  Counter App
+//
+//  Created by Sufian Bin Idris on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Counter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
